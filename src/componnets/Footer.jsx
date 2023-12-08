424ca6436867bc372e384949a76e8eef4db4a5af
Nurzhan Bizhan <bizhannk@gmail.com>
2023-12-08 19:46:17 +0600

    fix

--- src/componnets/Footer.jsx
+++ src/componnets/Footer.jsx
@@ -3,7 +3,7 @@ import { StyledFooter } from '../styles/Footer.styled'
 export const Footer = () => {
   return (
     <StyledFooter>
-      <a href='../../../Правила участия ПРОМО 2023.pdf'download>Правила участия</a>
+      <a href='../../../promo.pdf'download>Правила участия</a>
     </StyledFooter>
   )
 }
